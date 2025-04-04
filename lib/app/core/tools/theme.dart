bf4dfecbb0cba66348e95c1eaaec57d695c88c5a
christdev37 <juniormalcom912@gmail.com>
2025-04-04 15:17:56 +0100

    Build hero section

--- lib/app/core/tools/theme.dart
+++ lib/app/core/tools/theme.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 class StaticColors {
+  StaticColors._();
+
   static const red = Color(0xffDF6951);
   static const darkPrimaryText = Color(0xff212832);
   static const darkSecondaryText = Color(0xff5E6282);
   static const yellowColor = Color(0xffF1A501);
   static const greyTextColor = Color(0xff686D77);
-  static const sectionHeadigColor = Color(0xff14183E);
+  static const sectionHeadingColor = Color(0xff14183E);
 }
--- lib/app/features/home/home.dart
+++ lib/app/features/home/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:travel_landing/app/core/tools/responsive_breakpoint.dart';
 import 'package:travel_landing/app/core/tools/theme.dart';
+import 'package:travel_landing/app/features/home/widgets/category-section.dart';
 
 class HomeScreenn extends StatelessWidget {
   const HomeScreenn({super.key});
@@ -13,8 +14,15 @@ class HomeScreenn extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(
+            height: size.height,
             constraints: BoxConstraints(
               maxWidth: size.width > 1440 ? 1440 : size.width * 1,
+            ),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage("assets/images/fullcover.png"),
+              ),
             ),
 
             child: Responsive(
@@ -30,7 +38,7 @@ class HomeScreenn extends StatelessWidget {
 }
 
 class _HomeScreenMobile extends StatelessWidget {
-  const _HomeScreenMobile({super.key});
+  const _HomeScreenMobile();
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +47,7 @@ class _HomeScreenMobile extends StatelessWidget {
 }
 
 class _HomeScreenTablette extends StatelessWidget {
-  const _HomeScreenTablette({super.key});
+  const _HomeScreenTablette();
 
   @override
   Widget build(BuildContext context) {
@@ -48,58 +56,231 @@ class _HomeScreenTablette extends StatelessWidget {
 }
 
 class _HomeScreenWeb extends StatelessWidget {
-  const _HomeScreenWeb({super.key});
+  const _HomeScreenWeb();
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        return ListView(
-          children: [
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Image.asset("assets/images/Logo.png"),
-                  Container(
-                    width: constraints.maxWidth * 0.75,
-                    color: Colors.red,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        TextButton(
-                          onPressed: () {},
-                          child: Text("Destination"),
-                        ),
-                        TextButton(onPressed: () {}, child: Text("Hotel")),
-                        TextButton(onPressed: () {}, child: Text("Flight")),
-                        TextButton(onPressed: () {}, child: Text("Bookings")),
-                        TextButton(onPressed: () {}, child: Text("Login")),
-                        OutlinedButton(
-                          onPressed: () {},
-                          child: Text("Sign Up"),
-                          style: OutlinedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              side: BorderSide(
+        return SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: constraints.maxHeight * 0.06,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.asset("assets/images/Logo.png"),
+                    SizedBox(
+                      width: constraints.maxWidth * 0.75,
+                      // color: Colors.red,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Destination",
+                              style: TextStyle(
+                                fontSize: 17,
                                 color: StaticColors.darkPrimaryText,
+                                fontWeight: FontWeight.w400,
                               ),
-                              borderRadius: BorderRadius.circular(5),
                             ),
                           ),
-                        ),
-                        TextButton.icon(
-                          onPressed: () {},
-                          label: Text("EN"),
-                          icon: Icon(Icons.keyboard_arrow_down_outlined),
-                          iconAlignment: IconAlignment.end,
-                        ),
-                      ],
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Hotel",
+                              style: TextStyle(
+                                fontSize: 17,
+                                color: StaticColors.darkPrimaryText,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Flight",
+                              style: TextStyle(
+                                fontSize: 17,
+                                color: StaticColors.darkPrimaryText,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Bookings",
+                              style: TextStyle(
+                                fontSize: 17,
+                                color: StaticColors.darkPrimaryText,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Login",
+                              style: TextStyle(
+                                fontSize: 17,
+                                color: StaticColors.darkPrimaryText,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ),
+                          OutlinedButton(
+                            onPressed: () {},
+                            style: OutlinedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                side: BorderSide(
+                                  color: StaticColors.darkPrimaryText,
+                                ),
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                            ),
+                            child: Text(
+                              "Sign Up",
+                              style: TextStyle(
+                                fontSize: 17,
+                                color: StaticColors.darkPrimaryText,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ),
+                          TextButton.icon(
+                            onPressed: () {},
+                            label: Text(
+                              "EN",
+                              style: TextStyle(
+                                fontSize: 17,
+                                color: StaticColors.darkPrimaryText,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                            icon: Icon(Icons.keyboard_arrow_down_outlined),
+                            iconAlignment: IconAlignment.end,
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+              SizedBox(
+                height: constraints.maxHeight * 0.8,
+                child: Row(
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: LayoutBuilder(
+                        builder: (context, size) {
+                          return Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Best Destinations around the world"
+                                    .toUpperCase(),
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w700,
+                                  color: StaticColors.red,
+                                ),
+                              ),
+                              SizedBox(height: constraints.maxHeight * 0.03),
+                              SizedBox(
+                                width: constraints.maxWidth * 0.42,
+                                child: Stack(
+                                  children: [
+                                    Positioned(
+                                      right: -70,
+                                      top: constraints.maxHeight * 0.065,
+                                      child: Image.asset(
+                                        "assets/images/Decore.png",
+                                      ),
+                                    ),
+                                    Text(
+                                      "Travel, enjoy and live a new and full life",
+                                      maxLines: 3,
+                                      softWrap: true,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(
+                                        height: 1.1,
+                                        fontSize: 84,
+                                        fontWeight: FontWeight.w700,
+                                        color: StaticColors.darkPrimaryText,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(height: constraints.maxHeight * 0.03),
+                              SizedBox(
+                                height: 100,
+                                width: size.maxWidth * 0.6,
+                                child: Text(
+                                  "Built Wicket longer admire do barton vanity itself do in it. Preferred to sportsmen it engrossed listening. Park gate sell they west hard for the.",
+                                  softWrap: true,
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 4,
+                                  style: TextStyle(
+                                    height: 1.6,
+                                    fontSize: 16,
+
+                                    fontWeight: FontWeight.w400,
+                                    color: StaticColors.darkSecondaryText,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(height: constraints.maxHeight * 0.02),
+                              Row(
+                                children: [
+                                  ElevatedButton(
+                                    onPressed: () {},
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: StaticColors.yellowColor,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(20.0),
+                                      child: Text(
+                                        "Find out more",
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          );
+                        },
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage("assets/images/travailleur.png"),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(height: 40),
+              CategorySection(),
+            ],
+          ),
         );
       },
     );
